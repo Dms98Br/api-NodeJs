277092b849f80ec2275190c2e0def9d897c5a0b5
Dms98Br <54338660+Dms98Br@users.noreply.github.com>
2020-04-18 19:33:46 -0300

    Update keysaccess.js

--- src/configure/keysaccess.js
+++ src/configure/keysaccess.js
@@ -2,6 +2,6 @@
 module.exports = {
    
     connectionString:'mongodb+srv://ProFinder:profinder@cluster0-y6z2e.azure.mongodb.net/test',
-    sendgridKey: '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',
+    sendgridKey: '',
     containerConnectionString: 'TBD'
 }
